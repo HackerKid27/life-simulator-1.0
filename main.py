4abcdb387367dd64b5ce598dc639824b71ea5449
HackerKid27 <jdpellegrin2027@gmail.com>
2024-10-26 12:02:00 -0500

    Slight formatting changes to main python file
    Reorganized carnivore class and subclass to be more concise.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from random import randint as rand
 import math as m
 import time as t
 
+
 from pygame.locals import (
     QUIT,
     K_ESCAPE,
@@ -22,15 +23,6 @@ class Carnivore(pygame.sprite.Sprite):
         self.dir = rand(0, 179)
         self.speed = rand(2, 5)
         
-        
-class MaleCarn(Carnivore):
-    
-    def __init__(self):
-        super(MaleCarn, self).__init__()
-        self.surf.fill((255,0,0))
-        self.life = t.time()
-        self.life_span = rand(10, 30)
-        
     def update(self):
         if rand(1, 20) == 20:
             self.dir += rand(-45, 45)
@@ -51,6 +43,18 @@ class MaleCarn(Carnivore):
             self.dir += 180
         if t.time()-self.life >= self.life_span:
             self.kill()
+
+        
+class MaleCarn(Carnivore):
+    
+    def __init__(self):
+        super(MaleCarn, self).__init__()
+        self.surf.fill((255,0,0))
+        self.life = t.time()
+        self.life_span = rand(10, 30)
+        
+    def update(self):
+        super(MaleCarn, self).update()
 
 class FemaleCarn(Carnivore):
     
@@ -62,23 +66,7 @@ class FemaleCarn(Carnivore):
         self.life_span = rand(10, 30)
         
     def update(self):
-        if rand(1, 20) == 20:
-            self.dir += rand(-45, 45)
-        x_update = m.cos(self.dir*(m.pi/180))*self.speed
-        y_update = m.sin(self.dir*(m.pi/180))*self.speed
-        self.rect.move_ip(x_update, y_update)
-        if self.rect.left < 0:
-            self.rect.left = 0
-            self.dir += 180
-        if self.rect.right > 1000:
-            self.rect.right = 1000
-            self.dir += 180
-        if self.rect.top <= 0:
-            self.rect.top = 0
-            self.dir += 180
-        if self.rect.bottom >= 750:
-            self.rect.bottom = 750
-            self.dir += 180
+        super(FemaleCarn, self).update()
         if pygame.sprite.spritecollideany(self, male_carns):
             if not self.pregnant:
                 self.pregnant = True
@@ -86,8 +74,6 @@ class FemaleCarn(Carnivore):
                 self.due = rand(2, 7)
         if self.pregnant and t.time()-self.conception >= self.due:
             self.birth()
-        if t.time()-self.life >= self.life_span:
-            self.kill()
         
 
     def birth(self):
@@ -96,6 +82,7 @@ class FemaleCarn(Carnivore):
         else:
             pygame.event.post(pygame.event.Event(FEMALE_CARN))
         self.pregnant = False
+
 
 screen = pygame.display.set_mode((1000, 750))
 screen.fill((0,0,0))
