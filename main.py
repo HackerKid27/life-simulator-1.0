5df36c33108f36fa53649a42e26f91bb34a115e8
HackerKid27 <jdpellegrin2027@gmail.com>
2024-10-26 13:53:55 -0500

    Add another species to the simulator
    The species do not interact - yet.

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ class Carnivore(pygame.sprite.Sprite):
             self.speed = rand(2, 5)
             self.life_span = rand(10, 30)        
         
-        
     def update(self):
         if rand(1, 20) == 20:
             self.dir += rand(-45, 45)
@@ -74,8 +73,7 @@ class FemaleCarn(Carnivore):
                 self.conception = t.time()
                 self.due = rand(2, 7)
         if self.pregnant and t.time()-self.conception >= self.due:
-            self.birth()
-        
+            self.birth()        
 
     def birth(self):
         if rand(1, 7) < 4:
@@ -91,22 +89,106 @@ class FemaleCarn(Carnivore):
         self.pregnant = False
 
 
+class Herbivore(pygame.sprite.Sprite):
+    def __init__(self, parent=None):
+        super(Herbivore, self).__init__()
+        self.surf = pygame.Surface((50, 50))
+        self.rect = self.surf.get_rect()
+        self.dir = rand(0, 179)
+        self.life = t.time()
+        if parent != None:
+            self.speed = rand(parent.speed - 2, parent.speed + 2)
+            self.life_span = rand(parent.life_span - 10, parent.life_span +10)
+        else:
+            self.speed = rand(2, 5)
+            self.life_span = rand(10, 30)        
+        
+    def update(self):
+        if rand(1, 20) == 20:
+            self.dir += rand(-45, 45)
+        x_update = m.cos(self.dir*(m.pi/180))*self.speed
+        y_update = m.sin(self.dir*(m.pi/180))*self.speed
+        self.rect.move_ip(x_update, y_update)
+        if self.rect.left < 0:
+            self.rect.left = 0
+            self.dir += 180
+        if self.rect.right > 1000:
+            self.rect.right = 1000
+            self.dir += 180
+        if self.rect.top <= 0:
+            self.rect.top = 0
+            self.dir += 180
+        if self.rect.bottom >= 750:
+            self.rect.bottom = 750
+            self.dir += 180
+        if t.time()-self.life >= self.life_span:
+            self.kill()
+
+        
+class MaleHerb(Herbivore):
+    
+    def __init__(self, parent=None):
+        super(MaleHerb, self).__init__()
+        self.surf.fill((0,0,255))
+        
+    def update(self):
+        super(MaleHerb, self).update()
+
+
+class FemaleHerb(Herbivore):
+    
+    def __init__(self, parent=None):
+        super(FemaleHerb, self).__init__()
+        self.surf.fill((0,255,0))
+        self.pregnant = False
+        
+    def update(self):
+        super(FemaleHerb, self).update()
+        if pygame.sprite.spritecollideany(self, male_herbs):
+            if not self.pregnant:
+                self.pregnant = True
+                self.conception = t.time()
+                self.due = rand(2, 7)
+        if self.pregnant and t.time()-self.conception >= self.due:
+            self.birth()        
+
+    def birth(self):
+        if rand(1, 7) < 4:
+            new_herb = MaleHerb(self)
+            male_herbs.add(new_herb)
+            herbivores.add(new_herb)
+            organisms.add(new_herb)
+        else:
+            new_herb = FemaleHerb(self)
+            female_herbs.add(new_herb)
+            herbivores.add(new_herb)
+            organisms.add(new_herb)
+        self.pregnant = False
+
+
 screen = pygame.display.set_mode((1000, 750))
 screen.fill((0,0,0))
 
 organisms = pygame.sprite.Group()
 carnivores = pygame.sprite.Group()
+herbivores = pygame.sprite.Group()
 male_carns = pygame.sprite.Group()
 female_carns = pygame.sprite.Group()
-carnivores.add([male_carns, female_carns])
-organisms.add(carnivores)
+male_herbs = pygame.sprite.Group()
+female_herbs = pygame.sprite.Group()
 
-adam = MaleCarn()
-eve = FemaleCarn()
-male_carns.add(adam)
-female_carns.add(eve)
-carnivores.add(adam, eve)
-organisms.add(adam, eve)
+
+tiger = MaleCarn()
+tigeress = FemaleCarn()
+buck = MaleHerb()
+doe = FemaleHerb()
+male_carns.add(tiger)
+female_carns.add(tigeress)
+male_herbs.add(buck)
+female_herbs.add(doe)
+carnivores.add(tiger, tigeress)
+herbivores.add(buck, doe)
+organisms.add(tiger, tigeress, buck, doe)
 
 
 running = True
